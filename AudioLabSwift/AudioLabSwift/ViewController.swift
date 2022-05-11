12007ca3f99fac249b4fbde8a764676742aeed18
gzccc <zichengg@uci.edu>
2022-05-10 23:58:27 -0500

    AudioFlip

---
+++ AudioLabSwift/AudioLabSwift/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  AudioLabSwift
+//
+//  Created by Eric Larson 
+//  Copyright © 2020 Eric Larson. All rights reserved.
+//
+
+import UIKit
+import Metal
+
+
+let AUDIO_BUFFER_SIZE = 1024*4
+
+
+class ViewController: UIViewController {
+    
+    let audio = AudioModel(buffer_size: AUDIO_BUFFER_SIZE)
+    lazy var graph:MetalGraph? = {
+        return MetalGraph(mainView: self.view)
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // add in graphs for display
+        graph?.addGraph(withName: "fft",
+                        shouldNormalize: true,
+                        numPointsInGraph: AUDIO_BUFFER_SIZE/2)
+        
+        graph?.addGraph(withName: "fft_zoomed",
+                        shouldNormalize: true,
+                        numPointsInGraph: 20)
+        
+        graph?.addGraph(withName: "time",
+            shouldNormalize: false,
+            numPointsInGraph: AUDIO_BUFFER_SIZE)
+            //!!!!!!!!!!!!Uncomment line 42-43 and comment line 41 for microphone input!
+            audio.startProcesingAudioFileForPlayback(withFps: 10)
+            //audio.startMicrophoneProcessing(withFps: 10)
+            //audio.startProcessingSinewaveForPlayback(withFreq: 630.0)
+        
+            audio.play()
+
+        // run the loop for updating the graph peridocially
+        Timer.scheduledTimer(timeInterval: 0.05, target: self,
+            selector: #selector(self.updateGraph),
+            userInfo: nil,
+            repeats: true)
+       
+    }
+    
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        audio.pause()
+        print("paused")
+    }
+    
+    
+    @objc
+    func updateGraph(){
+        self.graph?.updateGraph(
+            data: self.audio.fftData,
+            forKey: "fft"
+        )
+        
+        self.graph?.updateGraph(
+            data: self.audio.fftData_zoomed,
+            forKey: "fft_zoomed"
+        )
+        
+        self.graph?.updateGraph(
+            data: self.audio.timeData,
+            forKey: "time"
+        )
+        
+    }
+    
+    
+
+}
+
